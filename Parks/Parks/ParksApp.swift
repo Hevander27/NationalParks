47cf55a4985afb03529c4df7d6f7fb71d1868f51
Hevander27 <45948489+Hevander27@users.noreply.github.com>
2024-03-23 22:25:56 -0400

    Add files via upload

---
+++ Parks/Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Admin on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
